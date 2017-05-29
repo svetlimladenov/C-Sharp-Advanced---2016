928d27a0be6e98ca6d2c665c8dc960b2b4bdd8fa
Svetli Mladenov <svetlimladenov@gmai.com>
2017-05-29 09:07:55 +0300

    adding Sets and Dictionaries - Exercises

--- 02.Sets and Dictionaries - Exercises/02. Sets of Elements/Program.cs
+++ 02.Sets and Dictionaries - Exercises/02. Sets of Elements/Program.cs
@@ -10,9 +10,23 @@ namespace _02.Sets_of_Elements
     {
         public static void Main(string[] args)
         {
-            var sets = Console.ReadLine();
-
-
+            var sets = Console.ReadLine().Split();
+            var n = int.Parse(sets[0]);
+            var m = int.Parse(sets[1]);
+            var firstSet = new List<int>();
+            var secondSet = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                var currentNum = int.Parse(Console.ReadLine());
+                firstSet.Add(currentNum);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                var currentNum = int.Parse(Console.ReadLine());
+                secondSet.Add(currentNum);
+            }
+            var result = firstSet.Intersect(secondSet);
+            Console.WriteLine(string.Join(" ",result));
         }
     }
 }
