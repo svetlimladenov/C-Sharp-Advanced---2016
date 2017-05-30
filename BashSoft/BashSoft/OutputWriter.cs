fb751fee91c7f4869c8459d7580fd8ee9eb5411c
Svetli Mladenov <svetlimladenov@gmai.com>
2017-05-31 00:30:24 +0300

    BashSoft organised

---
+++ BashSoft/BashSoft/OutputWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public static class OutputWriter
+    {
+        public static void WriteMessage(string message)
+        {
+        }
+
+        public static void WriteMessageOnNewLine(string message)
+        {
+        }
+
+        public static void WriteEmptyLine()
+        {        
+        }
+
+        public static void DisplayExeptions(string message)
+        {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = currentColor;
+        }
+
+        public static void TraverseFolder(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            int initialIdentation = path.Split('\\').Length;
+            Queue<string> subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+        }
+    }
+}
