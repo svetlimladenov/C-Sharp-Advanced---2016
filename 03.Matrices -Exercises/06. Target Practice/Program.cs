3c321b15fbd5f9cf7f0d79b709fc43b2aa62e064
Svetli Mladenov <svetlimladenov@gmai.com>
2017-06-03 00:32:02 +0300

    updating my homeworks

---
+++ 03.Matrices -Exercises/06. Target Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Target_Practice
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var sizeOfMatrix = Console.ReadLine()
+                .Split(new[] {' '},StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            char[][] matrix = new char[sizeOfMatrix[0]][];
+            var text = Console.ReadLine();
+            for (int row = matrix.Length - 1; row >= 0; row--)
+            {
+                matrix[row] = new char[sizeOfMatrix[1]];
+                foreach (var letter in text)
+                {                   
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
